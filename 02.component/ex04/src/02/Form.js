f69054db0d818e77a96a1a51836c01c5ab03e6e7
jung-jinwoo <wlsdn960319@gmail.com>
2021-12-01 12:34:16 +0900

    state #finish

--- 02.component/ex04/src/02/Form.js
+++ 02.component/ex04/src/02/Form.js
@@ -8,6 +8,9 @@ export default function Form() {
     const [email, setEmail] = useState("");
     const [validEmail, setValidEmail] = useState(false);
     const [password, setPassword] = useState("");
+    const [gender, setGender] = useState("female");
+    const [birthYear, setBirthYear] = useState('1988');
+    const [agreeProv, setAgreeProv] = useState('no');
 
     const onChangeInputName = (e) => {
         // setName(e.target.value);
@@ -25,6 +28,20 @@ export default function Form() {
     const onChangeInputPassword = (e) => {
         setPassword(e.target.value);
     };
+
+    const onChangeInputGender = (e) => {
+        setGender(e.target.value);  
+    }
+
+    const onChangeInputProv = (e) => {
+        const status = e.target.value === 'no' ? 'yes' : 'no';
+        // API 호출
+        const url = `/prov/agree?status=${status}`;
+        console.log(url);
+        if(true){
+            setAgreeProv(status);
+        }
+    }
 
     return (
         <form id="joinForm" name="joinForm" method="post" action="/do/not/post">
@@ -45,12 +62,12 @@ export default function Form() {
 
             <fieldset>
                 <legend>성별</legend>
-                <label>여</label> <input type="radio" name="gender" value={ "female" } defaultChecked={ true } />
-                <label>남</label> <input type="radio" name="gender" value={ "male" } defaultChecked={ false } />
+                <label>여</label> <input type="radio" name="gender" value={ "female" } onChange={onChangeInputGender} checked={gender === "female"} defaultChecked={ true } />
+                <label>남</label> <input type="radio" name="gender" value={ "male" } onChange={onChangeInputGender} checked={gender === "male"} defaultChecked={ false } />
             </fieldset>
 
             <label htmlFor="birthYear">생년</label>
-            <select id="birthYear">
+            <select id="birthYear" value={birthYear} onChange={(e) => {setBirthYear(e.target.value)}} >
                 <option value='1984'>1984년</option>
                 <option value='1985'>1985년</option>
                 <option value='1986'>1986년</option>
@@ -65,7 +82,7 @@ export default function Form() {
 
             <fieldset>
                 <legend>약관동의</legend>
-                <input id="agree-prov" type="checkbox" name="agreeProv" value= { "yes" } defaultChecked={ false } />
+                <input id="agree-prov" type="checkbox" name="agreeProv" value= { agreeProv } checked={agreeProv === 'yes'} onChange={onChangeInputProv} defaultChecked={ false } />
                 <label>서비스 약관에 동의합니다.</label>
             </fieldset>
 
--- 02.component/ex04/src/03/Form.js
+++ 02.component/ex04/src/03/Form.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './assets/Form.css';
 
 export default function Form() {
+    const onSubmit = (e) => {
+        e.preventDefault();
 
+        // validation 이 필요가 크게 없을 때...
+        console.log(e.target.email.value, ':', e.target.password.value);
+
+        // ajax login
+    }
 
     return (
         <form
             id="loginForm"
             name="loginForm"
             method="post"
-            action="/do/not/post">
+            action="/do/not/post"
+            onSubmit = {onSubmit}>
             <label htmlFor="email">이메일</label>
             <input
                 id="email"
